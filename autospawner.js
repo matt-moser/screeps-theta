6adbc70f5e562b2b6dbe297712ef01d14ba35f94
Matt Moser <matt.moser@meredith.com>
2018-05-31 15:45:55 -0500

    use creep count instead of index to determine limits

--- autospawner.js
+++ autospawner.js
@@ -1,20 +1,19 @@
+const countByRole = function() {
+
+}
+
 const autoSpawner = function autoSpawner () {
     const desiredRoles = {
         harvester: 3,
         upgrader: 6,
         builder: 1
     }
-
-    const roleMap = {};
-    for (let creepName in Game.creeps) {
-        let creep = Game.creeps[creepName];
-        roleMap[creep.memory.role] = roleMap[creep.memory.role] ? roleMap[creep.memory.role] + 1 : 1;
-    }
-
-    let nextRole = 'harvester';
+    
     let spawn = false;
     for(let role in desiredRoles) {
-        if (!roleMap[role]  || roleMap[role] < desiredRoles[role]) {
+        roleCreeps = _.filter(Game.creeps, (creep) => creep.memory.role == role);
+
+        if (roleCreeps.length < desiredRoles[role]) {
             nextRole = role;
             spawn = true;
             break;
@@ -23,7 +22,7 @@ const autoSpawner = function autoSpawner () {
 
     if (spawn) {
         for(let name in Game.spawns) {
-            const newName = nextRole + (roleMap[nextRole] ? roleMap[nextRole]+1 : 1);
+            const newName = nextRole + Game.time;
             const result = Game.spawns[name].spawnCreep([WORK, CARRY, MOVE], newName, {
                 memory: {role: nextRole}
             })
