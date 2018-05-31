5fb7cdccc6396d44f709608d12365e1214344cfd
Matt Moser <matt.moser@meredith.com>
2018-05-31 14:33:14 -0500

    asdf

--- main.js
+++ main.js
@@ -24,7 +24,7 @@ module.exports.loop = function () {
             roleUpgrader.run(creep);
         }
         if(creep.memory.role == 'builder') {
-            builderManager.run(creep)
+            roleBuilder.run(creep)
         }
     }
 }
