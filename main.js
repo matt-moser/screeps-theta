a40136138531d382bc0b502333afca74ecf6d762
Matt Moser <matt.moser@meredith.com>
2018-05-31 14:06:51 -0500

    builder variable bugfix

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 var roleHarvester = require('role.harvester');
 var roleUpgrader = require('role.upgrader');
-var builderManager = require('role.builder');
+var roleBuilder = require('role.builder');
 var autoSpawner = require('autospawner');
 
 module.exports.loop = function () {
