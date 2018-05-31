57fb044bc49d185b82632a98f28a33119e6faeb9
Matt Moser <matt.moser@meredith.com>
2018-05-31 17:11:14 -0500

    better worker state machine

--- role.worker.js
+++ role.worker.js
@@ -36,10 +36,9 @@ const buildConstructionSite = function (creep) {
 
 module.exports = {
     run: function(creep){
-        if (creep.memory.building && creep.carry.energy = 0) {
+        if (creep.memory.building && creep.carry.energy === 0) {
             creep.memory.building = false
         }
-
         
         if (!creep.memory.building && creep.carry.energy < creep.carryCapacity) {
             creep.say('🔄 harvest');
@@ -51,7 +50,6 @@ module.exports = {
             creep.memory.harvesting = false;
             creep.memory.building = true;
         }
-
 
         if (creep.memory.harvesting) {
             
