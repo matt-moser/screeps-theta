46d2cb2f085edf38f5da6f26e647e952ee6d9a11
Ben H <ben.a.hammond@gmail.com>
2018-05-31 14:25:50 -0700

    reworking to workers

---
+++ role.worker.js
@@ -0,0 +1,38 @@
+module.exports = {
+    run: function(creep){
+
+        if (creep.carry.energy < creep.carryCapacity) {
+            creep.say('🔄 harvest');
+            var source = creep.pos.findClosestByPath(FIND_SOURCES);
+            if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}});
+            }
+        } else {
+            if (extensions.length < 1) {
+                creep.say('🚧 build');
+                var targets = creep.room.findClosestByPath(FIND_CONSTRUCTION_SITES);
+                if(targets.length) {
+                    if(creep.build(targets[0]) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(targets[0], {visualizePathStyle: {stroke: '#ffffff'}});
+                    }
+                }
+            } else {
+                creep.say('🔄 return');
+                var target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
+                    filter: (structure) => {
+                        return (structure.structureType == STRUCTURE_EXTENSION || structure.structureType == STRUCTURE_SPAWN) &&
+                            structure.energy < structure.energyCapacity;
+                    }
+                });
+                if (target) {
+                    if(creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                        creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
+                    }
+                }
+                else{
+                    creep.say('⁉️ Nowhere to return resources and nothing to build');
+                }
+            }
+        }
+    }
+}
