66e1f95093e4c1a8c6989dc58e15006ad5d78e8a
Matt Moser <matt.moser@meredith.com>
2018-05-31 15:03:43 -0500

    more alerting

--- role.harvester.js
+++ role.harvester.js
@@ -2,7 +2,18 @@ var roleHarvester = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-	    if(creep.carry.energy < creep.carryCapacity) {
+
+	    if(!creep.memory.harvesting && creep.carry.energy < creep.carryCapacity) {
+            creep.memory.harvesting = true
+            creep.say('🔄 harvest');
+        }
+        if(creep.memory.harvesting && creep.carry.energy >= creep.carryCapacity) {
+            creep.memory.harvesting = false
+            creep.say('🔄 full');
+        }
+
+
+	    if(creep.memory.harvesting) {
             var source = creep.pos.findClosestByPath(FIND_SOURCES);
             if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}});
